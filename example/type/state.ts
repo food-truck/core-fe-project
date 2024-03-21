723915a2fac39b6c10b0dc569a064533334f3f68
Engelce <engelliu@chancetop.com>
2024-03-21 17:17:42 +0800

    TIW-21 add vite server

--- example/type/state.ts
+++ example/type/state.ts
@@ -1,5 +1,5 @@
 import {State} from "../../src";
-import {State as MainState} from "../page/main";
+import {State as MainState} from "../page/main/type";
 import {State as TemplateState} from "../page/Template/type";
 
 export interface RootState extends State {
