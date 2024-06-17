06886f3e9ed918c2979ca9bf5bfa8ad97f3a93b2
yorkerxu <yorkerxu@xm.wonder.com>
2024-06-17 10:36:31 +0800

    Add loading initial value

--- src/decorator/Loading.ts
+++ src/decorator/Loading.ts
@@ -3,7 +3,14 @@ import {setLoadingState, createActionHandlerDecorator, type ActionHandlerWithMet
 /**
  * To mark state.loading[identifier] during action execution.
  */
-export function Loading<ReturnType>(identifier: string = "global") {
+export function Loading<ReturnType>(identifier: string = "global", initialLoading?: boolean) {
+    if (initialLoading) {
+        setLoadingState({
+            identifier,
+            show: initialLoading,
+        });
+    }
+
     return createActionHandlerDecorator(async function (handler: ActionHandlerWithMetaData<ReturnType>) {
         setLoadingState({
             identifier,
